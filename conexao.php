052eeb2fb2fd5a7935d70a6a0036fe1014303d38
leonardo <leopardorocha@gmail.com>
2025-04-16 12:21:50 -0300

    ESTILIZAÇÃO E MIGRATE ATUALIZADA

--- conexao.php
+++ conexao.php
@@ -36,9 +36,11 @@
         CREATE TABLE alunos (
             id SERIAL PRIMARY KEY,
             nome VARCHAR(100) NOT NULL,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            senha VARCHAR(255) NOT NULL,
-            criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            data_nascimento DATE NOT NULL,
+            cpf VARCHAR(255) NOT NULL,
+            telefone VARCHAR(255) NOT NULL,
+            whatsapp VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
         );
     ";
     checkAndCreateTable($database, 'alunos', $createAlunosTable);
@@ -46,9 +48,7 @@
     $createCursosTable = "
         CREATE TABLE cursos (
             id SERIAL PRIMARY KEY,
-            nome VARCHAR(100) NOT NULL,
-            descricao TEXT,
-            criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            nome VARCHAR(100) NOT NULL
         );
     ";
     checkAndCreateTable($database, 'cursos', $createCursosTable);
@@ -57,8 +57,7 @@
         CREATE TABLE alunos_cursos (
             id SERIAL PRIMARY KEY,
             aluno_id INT NOT NULL REFERENCES alunos(id) ON DELETE CASCADE,
-            curso_id INT NOT NULL REFERENCES cursos(id) ON DELETE CASCADE,
-            inscrito_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            curso_id INT NOT NULL REFERENCES cursos(id) ON DELETE CASCADE
         );
     ";
     checkAndCreateTable($database, 'alunos_cursos', $createAlunosCursosTable);
